66440f4777bb9d85d15f488267ac4c2ab2192ec7
Jadon Fowler <jadonflower@gmail.com>
2016-08-22 21:35:17 -0700

    Store Chunks and Block modifications
    Chat messages are also printed out to the console.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main/kotlin/xyz/jadonfowler/pabot/Pabot.kt
+++ src/main/kotlin/xyz/jadonfowler/pabot/Pabot.kt
@@ -2,17 +2,21 @@ package xyz.jadonfowler.pabot
 
 import org.spacehq.mc.protocol.MinecraftConstants
 import org.spacehq.mc.protocol.MinecraftProtocol
+import org.spacehq.mc.protocol.data.game.chunk.Column
+import org.spacehq.mc.protocol.data.game.world.block.BlockChangeRecord
 import org.spacehq.packetlib.Client
 import org.spacehq.packetlib.tcp.TcpSessionFactory
 import java.awt.BorderLayout
 import java.awt.GridLayout
 import java.net.Proxy
+import java.util.*
 import javax.swing.*
 
 class Pabot(username: String, password: String, val host: String, val port: Int = 25565) {
 
     val client: Client
     var gameSettings: GameSettings? = null
+    val chunks: ArrayList<Column> = ArrayList()
 
     init {
         val protocol = MinecraftProtocol(username, password)
@@ -23,6 +27,23 @@ class Pabot(username: String, password: String, val host: String, val port: Int 
     fun login() {
         client.session.addListener(PacketHandler(this))
         client.session.connect()
+    }
+
+    fun getChunkColumn(x: Int, z: Int): Column? {
+        for (chunk in chunks) {
+            if (chunk.x == x && chunk.z == z) return chunk
+        }
+        return null
+    }
+
+    fun updateBlock(record: BlockChangeRecord) {
+        val chunkX: Int = record.position.x / 16
+        val chunkY: Int = record.position.y / 16
+        val chunkZ: Int = record.position.z / 16 // + (record.position.z > 0 ? 0 : 1) // maybe?
+        val blockX: Int = record.position.x % 16
+        val blockY: Int = record.position.y % 16
+        val blockZ: Int = record.position.z % 16
+        getChunkColumn(chunkX, chunkZ)!!.chunks[chunkY].blocks.set(blockX, blockY, blockZ, record.block)
     }
 
 }
--- src/main/kotlin/xyz/jadonfowler/pabot/PacketHandler.kt
+++ src/main/kotlin/xyz/jadonfowler/pabot/PacketHandler.kt
@@ -2,8 +2,11 @@ package xyz.jadonfowler.pabot
 
 import org.spacehq.mc.protocol.data.message.Message
 import org.spacehq.mc.protocol.packet.ingame.client.ClientChatPacket
+import org.spacehq.mc.protocol.packet.ingame.server.ServerChatPacket
 import org.spacehq.mc.protocol.packet.ingame.server.ServerJoinGamePacket
 import org.spacehq.mc.protocol.packet.ingame.server.scoreboard.ServerTeamPacket
+import org.spacehq.mc.protocol.packet.ingame.server.world.ServerBlockChangePacket
+import org.spacehq.mc.protocol.packet.ingame.server.world.ServerChunkDataPacket
 import org.spacehq.mc.protocol.packet.ingame.server.world.ServerMultiBlockChangePacket
 import org.spacehq.packetlib.event.session.*
 import org.spacehq.packetlib.packet.Packet
@@ -33,10 +36,19 @@ class PacketHandler(val bot: Pabot) : SessionAdapter() {
                 println(bot.gameSettings)
                 event.session.send(ClientChatPacket("Hello, I'm a bot!"))
             }
+            is ServerChatPacket -> {
+                println(packet.message.fullText)
+            }
             is ServerMultiBlockChangePacket -> {
                 for (record in packet.records) {
-                    println(record.position.toString() + " -> " + record.block.id)
+                    bot.updateBlock(record)
                 }
+            }
+            is ServerBlockChangePacket -> {
+                bot.updateBlock(packet.record)
+            }
+            is ServerChunkDataPacket -> {
+                bot.chunks.add(packet.column)
             }
         }
     }
